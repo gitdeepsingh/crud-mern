ef63f61a577dad1098e43fb4340f15f5ecc5a35b
Deep <deepsingh.cts@gmail.com>
2018-09-14 00:23:45 +0530

    set up mongoDB connection and making first fetch call to DB

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const fs = require('fs');
+const { MongoClient, ObjectID } = require('mongodb');
 
 const port = process.env.PORT || 4200;
+const urlDB = 'mongodb://localhost:27017';
 const app = express();
+
+//SETTING CORS
 const allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   next();
 }
 app.use(allowCrossDomain);
+
+//SETTING LOGS
 app.use((req, res, next) => {
   const now = new Date().toString();
   const log = `${now} ${req.method} ${req.url}`;
@@ -19,15 +25,34 @@ app.use((req, res, next) => {
   next();
 });
 
+//CONNECTING TO MONGODB
+let users = [];
+MongoClient.connect(urlDB, (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to DB server.');
+  }
+  console.log('Connected to DB server.');
+  const db = client.db('iDB');
+
+  db.collection('Inventory').find({ isSingle: false }).toArray().then((docs) => {
+    console.log(docs);
+    users = docs;
+  }, (err) => {
+    console.log('Unable to fetch Inventory', err)
+  });
+
+  client.close();
+});
+
+//SETTING APIs
 app.get('/', (req, res) => {
   res.send({
-    "welcomeText": 'Hey there! Welcome to the website.'
+    welText: 'Welcome User'
   })
 });
 app.get('/users', (req, res) => {
   res.send({
-    0: ['deep', 'katal'],
-    1: ['sidhu', 'sharma']
+    users
   })
 });
 
